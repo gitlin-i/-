1cdc2dd744b650c6a6dfaf2e1805cd0e7e491919
gitlin-i <seokjin0204@naver.com>
2020-08-31 12:26:10 +0900

    해시 1차풀이 그러나 효율성 테스트 실패

--- 프로그래머스 완주하지 못한 선수.py
+++ 프로그래머스 완주하지 못한 선수.py
@@ -1,24 +1,22 @@
 #프로그래머스 완주하지 못한 선수
 #https://programmers.co.kr/learn/courses/30/lessons/42576
-def solution(participant, completion): 
-    answer = participant.copy()
-    pre = completion.copy()
-    for i in range(0,len(pre)):
-        answer.remove(pre[i])
-    return answer
+def solution(participant, completion):
+    dic_a = {}
+    for i in range(0,len(participant)):
+        if ord(participant[i][0]) not in dic_a:
+            dic_a[ord(participant[i][0])]= [participant[i]]
+        else:
+            dic_a[ord(participant[i][0])].append(participant[i])
+    for j in range(0,len(completion)):
+        dic_a[ord(completion[j][0])].remove(completion[j])
+    for k in range(ord('a'),ord('z')+1):
+        if k in dic_a:
+            if len(dic_a[k]) == 1:
+                return dic_a[k][0]
 
-answer=solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])[0]
-print(answer)
+a=solution(['mislav', 'stanko', 'mislav', 'ana'],['stanko', 'ana', 'mislav'])
+print(a)
 
-#동명이인에 대한 처리 불가, 문제의 주제는 해시였음
-#해시를 통해 다시풀어보기
-#변수 = 리스트 -> 리스트 주소 값 복사
-#변수 = 리스트.copy() -> 리스트 값 복사
-#--------------------------
-#def solution(participant, completion): 
-#    for i in range(0,len(completion)):
-#        participant.remove(completion[i])
-#    return participant[0]
-#가장 적은 라인수 하지만 해시를 이용한 풀이는 아님
-#코드 제출 결과 정확성은 있지만 효율적인 풀이는 아님
-#즉, 해시를 이용해야함
+#첫번째 반복문으로 이름 첫글자로 키값을 정하고 첫글자가 같은 이름들을 리스트 형태로 넣어 딕셔너리값을 넣음
+#두번째 반복문으로 이름들을 지워나가면 키는 있으나 빈리스트와 이름 하나만 존재하는 리스트가 남겨짐
+#세번째 반복문으로 키가 존재하면서 리스트 길이가 1인 값을 찾아내 반환
