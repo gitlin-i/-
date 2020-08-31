324498290b9d162ffafedeb664f90cef37aac997
gitlin-i <seokjin0204@naver.com>
2020-08-31 16:02:13 +0900

    해시 2차 풀이, 드디어 정확성,효율성테스트 모두 통과 글자수와 첫글자를 이용해 풀음

--- 프로그래머스 완주하지 못한 선수.py
+++ 프로그래머스 완주하지 못한 선수.py
@@ -1,22 +1,22 @@
 #프로그래머스 완주하지 못한 선수
 #https://programmers.co.kr/learn/courses/30/lessons/42576
+def tuple_g(a):
+    return len(a),a[0]
+
 def solution(participant, completion):
-    dic_a = {}
+    dic_a={}
     for i in range(0,len(participant)):
-        if ord(participant[i][0]) not in dic_a:
-            dic_a[ord(participant[i][0])]= [participant[i]]
+        if tuple_g(participant[i]) not in dic_a:
+            dic_a[tuple_g(participant[i])] = [participant[i]]
         else:
-            dic_a[ord(participant[i][0])].append(participant[i])
+            dic_a[tuple_g(participant[i])].append(participant[i])
     for j in range(0,len(completion)):
-        dic_a[ord(completion[j][0])].remove(completion[j])
-    for k in range(ord('a'),ord('z')+1):
-        if k in dic_a:
-            if len(dic_a[k]) == 1:
-                return dic_a[k][0]
+        dic_a[tuple_g(completion[j])].remove(completion[j])
+        if dic_a[tuple_g(completion[j])] == []:
+            del dic_a[tuple_g(completion[j])]
+    for key in dic_a:
+        return dic_a[key][0]
 
-a=solution(['mislav', 'stanko', 'mislav', 'ana'],['stanko', 'ana', 'mislav'])
-print(a)
-
-#첫번째 반복문으로 이름 첫글자로 키값을 정하고 첫글자가 같은 이름들을 리스트 형태로 넣어 딕셔너리값을 넣음
-#두번째 반복문으로 이름들을 지워나가면 키는 있으나 빈리스트와 이름 하나만 존재하는 리스트가 남겨짐
-#세번째 반복문으로 키가 존재하면서 리스트 길이가 1인 값을 찾아내 반환
+#드디어 효율성테스트 통과
+#1~20자이며 첫 문자를 이용해 글자수,첫글자 튜플을 딕셔너리에 리스트형태로 분류
+#그 뒤 하나씩 값을 제거해 마지막에 남는 키 값을 출력
