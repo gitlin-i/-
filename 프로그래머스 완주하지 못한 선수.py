93513ab006078ef5c180a94b872d93a260ff4689
gitlin-i <seokjin0204@naver.com>
2020-08-31 11:27:03 +0900

    정확성은 있으나 효율적이지 못한 풀이

--- 프로그래머스 완주하지 못한 선수.py
+++ 프로그래머스 완주하지 못한 선수.py
@@ -2,16 +2,23 @@
 #https://programmers.co.kr/learn/courses/30/lessons/42576
 def solution(participant, completion): 
     answer = participant.copy()
-    for i in range(0,len(participant)):
-        for j in range(0,len(completion)) :
-            if participant[i] == completion[j]:
-                answer.remove(completion[j])
+    pre = completion.copy()
+    for i in range(0,len(pre)):
+        answer.remove(pre[i])
     return answer
 
-answer=solution(["mislab", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])[0]
+answer=solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"])[0]
 print(answer)
 
 #동명이인에 대한 처리 불가, 문제의 주제는 해시였음
 #해시를 통해 다시풀어보기
 #변수 = 리스트 -> 리스트 주소 값 복사
 #변수 = 리스트.copy() -> 리스트 값 복사
+#--------------------------
+#def solution(participant, completion): 
+#    for i in range(0,len(completion)):
+#        participant.remove(completion[i])
+#    return participant[0]
+#가장 적은 라인수 하지만 해시를 이용한 풀이는 아님
+#코드 제출 결과 정확성은 있지만 효율적인 풀이는 아님
+#즉, 해시를 이용해야함
