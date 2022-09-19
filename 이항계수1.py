d2988cbb0bd56c0001d4d65cb5cfba6d5a65b286
gitlin-i <70059525+gitlin-i@users.noreply.github.com>
2022-09-20 00:40:41 +0900

    1

---
+++ 이항계수1.py
@@ -0,0 +1,18 @@
+
+class BinomialCoefficient:
+    def __init__(self,N,k) -> None:
+        self.N = N
+        self.k = k 
+    def fac(self,n):
+        cumulative_product = 1
+        for i in range(1, n + 1):
+            cumulative_product *= i
+        return cumulative_product
+    
+    def get(self):
+        return self.fac(self.N) // (self.fac(self.N-self.k) * self.fac(self.k))
+
+n,k = map(int,input().split())
+
+a = BinomialCoefficient(n,k)
+print(a.get())
