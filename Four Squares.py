aa7382c058e10b19406bffcc046ad346d2931112
gitlin-i <70059525+gitlin-i@users.noreply.github.com>
2022-12-09 23:31:13 +0900

    11

--- Four Squares.py
+++ Four Squares.py
@@ -1,9 +1,9 @@
 import sys
 
-
 input = sys.stdin.readline
 
 n = int(input())
+
 
 memo = [0] * 50001
 memo[1] = 1
@@ -12,16 +12,20 @@ memo[3] = 3
 
 # 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23  
 # 1 2 3 1 2 3 4 2 1 2  3  3  2  3  4  1  2  2  3  2  3  3  4 
-for i in range(1,224):
-    memo[i*i] =1 
 
-a = [1,2]
-for i in range(4,50001):
-    if (a[-1] +1) **2 < i:
-        a.append(a[-1] +1)
+squares = [1]
 
-    for j in a:
-        memo[i] = min(memo[i-1]+1 , memo[i-j] + memo[j])
+
+for i in range(4,n+1):
+    if i == (len(squares) + 1)**2:
+        squares.append(i)
+        memo[i] = 1
+        continue
+    memo[i] = memo[i-1] +1
+
+    for j in squares:
+        memo[i] = min(memo[i], memo[i-j] + memo[j])
+
 
 print(memo[n])
 
@@ -30,3 +34,24 @@ print(memo[n])
 """
 boj_problem=17626
 """
+
+
+def four(n):
+
+
+
+    if int(n**(0.5)) == n**(0.5):
+        return 1
+
+    for i in range(1,int(n**(0.5)) +1):
+        if int((n- i**2)**(0.5)) == (n-i**2) ** (0.5):
+            return 2
+    
+    for i in range(1,int(n**(0.5))+1):
+        for j in range(1,int((n-i**2)**(0.5)) + 1):
+            if int((n-i**2-j**2)**(0.5)) == (n-i**2-j**2) ** (0.5):
+                return 4
+    return 4
+
+
+print('#',four(34567))
